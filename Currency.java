b68e0e1682b9ee4d53b36df96e5a55c122bd3187
VladRybkin <Footballer0@mail.ru>
2016-10-31 13:23:20 +0200

    VladCommit

---
+++ Currency.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Vlad on 30.10.2016.
+ */
+public enum Currency {
+    UAH(980), USD(840);
+
+    private int code;
+
+    Currency(int code) {
+        this.code = code;
+    }
+
+}
+
+
+
---
+++ RoomType.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Vlad on 30.10.2016.
+ */
+public enum RoomType {
+    Standart, Econom, Lux
+}
