021916feb41bd1a09bb7bcd525c3fe216e4b34a1
Oleg Small <smolovik.oleg@gmail.com>
2016-11-01 21:09:48 +0200

    edited DAO interface

--- DAO.java
+++ DAO.java
@@ -1,19 +1,13 @@
 /**
  * Created by Димон on 31.10.2016.
  */
-public interface DAO {
-/*
-    Room save(Room room);
-    boolean delete(Room room);
-    Room update(Room room);
-    Room findById(long id);
-    User save(User user);
-    boolean delete(User user);
-    User update(User user);
-    User findById(long id);
-    Hotel save(Hotel hotel);
-    boolean delete(Hotel hotel);
-    Hotel update(Hotel hotel);
-    Hotel findById(long id);
-*/
+interface DAO<T> {
+
+    T save(T t);
+
+    boolean delete(T t);
+
+    T update(T t);
+
+    T findById(long id);
 }
