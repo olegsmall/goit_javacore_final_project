5fe7b4058d771e7716c87cead5d6e11ff9d5ee97
Oleg Small <smolovik.oleg@gmail.com>
2016-11-01 21:56:07 +0200

    edited Controller

--- Controller.java
+++ Controller.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Администратор on 01.11.2016.
@@ -12,5 +13,17 @@ public class Controller {
     public List<Hotel> findHotelByCity(String city) {
 
         return null;
+    }
+
+    void bookRoom(long roomId, long userId, long hotelId) {
+
+    }
+
+    void cancelReservation(long roomId, long userId, long hotelId) {
+
+    }
+
+    List<Hotel> findRoom(Map<String, String> params) {
+
     }
 }
