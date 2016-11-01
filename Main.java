6092d31a64515ab5117c8e591cde0047ac6705f7
Oleg Small <smolovik.oleg@gmail.com>
2016-11-01 21:00:30 +0200

    adited

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
+import java.awt.*;
+import java.io.*;
+
 /**
  * Created by Администратор on 30.10.2016.
  */
 public class Main {
-    public static void main(String[] args) {
-        //System.out.println("dfdfdf");
+    public static void main(String[] args) throws IOException {
+
+
 
     }
 }
