ad63e2cd9c1228fcccee6745fb501f60c995498b
Oleg Small <smolovik.oleg@gmail.com>
2016-11-04 18:23:56 +0200

    smone changes in Controller

--- Controller.java
+++ Controller.java
@@ -13,7 +13,7 @@ public class Controller {
 
     public List<Hotel> findHotelByName(String name) {
         List<Hotel> hotels;
-        if (!isUserRegistered()){
+        if (!isUserRegistered()) {
             return null;
         }
         hotels = hotelDAO.findByName(name);
@@ -25,7 +25,7 @@ public class Controller {
 
     public List<Hotel> findHotelByCity(String city) {
         List<Hotel> hotels;
-        if (!isUserRegistered()){
+        if (!isUserRegistered()) {
             return null;
         }
         hotels = hotelDAO.findByCity(city);
@@ -44,33 +44,31 @@ public class Controller {
     }
 
     public void bookRoom(long roomId, long userId, long hotelId) {
+        // добавил проверку на регистрацию пользователя
+        if (!isUserRegistered()) {
+            return;
+        }
+
         try {
-            if (roomId == 0 && userId == 0 && hotelId == 0) {
-                throw new NullPointerException();
-            }
-
-            for (int i = 0; i < hotelDAO.db.size(); i++) {
-                for (int j = 0; j < hotelDAO.db.get(i).getRooms().size(); j++) {
-                    if (hotelDAO.db.get(i).getRooms().get(j).getId() == roomId
-                            && CurrentUser.getCurrentUser().getId() == userId
-                            && hotelDAO.db.get(i).getId() == hotelId) {
-                        hotelDAO.db.get(i).getRooms().get(j).setReservedForUser(CurrentUser.getCurrentUser());
-                    }
-
-
+                // Изменил условие на ИЛИ, было И, получалось что исключение кидалось только если все поля были нулевыми
+                if (roomId == 0 || userId == 0 || hotelId == 0) {
+                    throw new NullPointerException();
                 }
-
-
-            }
-
-
-        } catch (NullPointerException e
-                ) {
+                //это нужно заменить на getById для соответствующих классов
+                for (int i = 0; i < hotelDAO.db.size(); i++) {
+                    for (int j = 0; j < hotelDAO.db.get(i).getRooms().size(); j++) {
+                        if (hotelDAO.db.get(i).getRooms().get(j).getId() == roomId
+                                && CurrentUser.getCurrentUser().getId() == userId
+                                && hotelDAO.db.get(i).getId() == hotelId) {
+                            hotelDAO.db.get(i).getRooms().get(j).setReservedForUser(CurrentUser.getCurrentUser());
+                        }
+                    }
+                }
+        } catch (NullPointerException e) {
             System.out.println("Yours id's aren't correct");
         } catch (IndexOutOfBoundsException e) {
             System.out.println("There is IndexOfBoundException");
         }
-
     }
 
     public void cancelReservation(long roomId, long userId, long hotelId) {
