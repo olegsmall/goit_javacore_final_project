b2563d2b0ef58423fc6f49e2e11c063c02cd7c5b
Oleg Small <smolovik.oleg@gmail.com>
2016-11-06 21:24:40 +0200

    fixed user Dao

--- UserDAO.java
+++ UserDAO.java
@@ -4,24 +4,18 @@ import java.util.List;
 /**
  * Created by Sergiy on 11/6/16.
  */
-
-public class UserDAO<T extends BaseEntity> extends DAO<User> {
-
-    List<T> userList = new ArrayList<>();
-
-        public T save(T t) {
-            userList.add(t);
-            return t;
-        }
+public class UserDAO extends DAO<User> {
 
 
-        public T findUserById(long id) { 
+/*
+        public T findUserById(long id) {
             for (T item : userList) {
                 if (item.getId() == id)
                     return item;
             }
             return null;
         }
+*/
     }
 
 
